471897185404c2d5c0c0cb92cc997f911642c785
aspiringguru <bmatthewtaylor@gmail.com>
2020-02-02 08:33:56 +1000

    wip, basic json hellow world

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello_world():
+    return jsonify('hello, world!')
+
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=5000)
